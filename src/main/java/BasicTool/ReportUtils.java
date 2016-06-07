8616b0b420dee4a6baf5b5f9683bf43fcfdb356b
Alex yang <2722223@qq.com>
2016-06-08 01:11:59 +0800

    support click img and update cases

--- src/main/java/BasicTool/ReportUtils.java
+++ src/main/java/BasicTool/ReportUtils.java
@@ -7,24 +7,31 @@ import org.uncommons.reportng.ReportNGUtils;
 
 public class ReportUtils extends ReportNGUtils {
 
-    public List<String> getTestOutput(final ITestResult result) {
-        final List<String> output = super.getTestOutput(result);
-        final Exception error = (Exception) result.getAttribute("reportGeneratingException");
+	public List<String> getTestOutput(final ITestResult result) {
+		final List<String> output = super.getTestOutput(result);
+		final Exception error = (Exception) result
+				.getAttribute("reportGeneratingException");
 
-        if (error != null) {
-            output.add("Generating report error: " + error);
-            return output;
-        }
-        List<String> attribute = (List<String>) result.getAttribute("screenshots");
-        List<String> screenshots = attribute;
-        if (!screenshots.isEmpty()) {
-            for (String str : screenshots) {
-                // String str1 = "<a href=\"../../" + str + "\" target=\"_blank\">Screenshot</a>";
-
-                String str1 = "<li><img src=\"../../" + str + "\" alt=\"\"+ data-large-src=\"../../" + str + "\"></li>";
-                output.add(str1);
-            }
-        }
-        return output;
-    }
+		if (error != null) {
+			output.add("Generating report error: " + error);
+			return output;
+		}
+		List<String> attribute = (List<String>) result
+				.getAttribute("screenshots");
+		List<String> screenshots = attribute;
+		if (!screenshots.isEmpty()) {
+			for (String str : screenshots) {
+				// String str1 = "<a href=\"../../" + str +
+				// "\" target=\"_blank\">Screenshot</a>";
+				// String str1 = "<li><img src=\"../../" + str
+				// + "\" alt=\"\"+ data-large-src=\"../../" + str
+				// + "\"></li>";
+				String str1 = "<li><a href=\"../../" + str
+						+ "\" target=\"_blank\"><img src=\"../../" + str
+						+ "\" alt=\"\"></a></li>";
+				output.add(str1);
+			}
+		}
+		return output;
+	}
 }
--- src/test/java/BasicTool/Mytest.java
+++ src/test/java/BasicTool/Mytest.java
@@ -1,59 +1,127 @@
 package BasicTool;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Mytest {
-    public WebDriver wd;
+	public WebDriver wd;
 
-    @BeforeMethod
-    public void init() {
-        wd = DriverFactory.createNewDriver();
-    }
+	@FindBy(css = "#kw")
+	private WebElement searchTextField;
 
-    @Test
-    public void MyfristTest5() throws InterruptedException {
-        wd.get("http://www.jiedaibao.com/pcIndex.html");
-        Thread.sleep(5000);
-        BaseHtmlReport.createScreenshots(wd);
-        wd.get("http://www.baidu.com");
-        Thread.sleep(5000);
-        BaseHtmlReport.createScreenshots(wd);
-        wd.get("http://www.bing.com");
-        Thread.sleep(10000);
-        BaseHtmlReport.createScreenshots(wd);
-    }
+	@FindBy(css = "#su")
+	private WebElement clickButton;
 
-     //@Test
-    public void MyfristTest6() throws InterruptedException {
-        wd.get("http://www.baidu.com");
-        Thread.sleep(5000);
-        BaseHtmlReport.createScreenshots(wd);
-        wd.get("http://www.taobao.com");
-        Thread.sleep(10000);
-        BaseHtmlReport.createScreenshots(wd);
+	@FindBy(css = "#sb_form_q")
+	private WebElement bSearchTextField;
 
-    }
+	@FindBy(css = "#sb_form_go")
+	private WebElement bClickButton;
 
-    // @Test
-    public void MyfristTest7() throws InterruptedException {
-        wd.get("http://www.baidu.com");
-        Thread.sleep(5000);
-        BaseHtmlReport.createScreenshots(wd);
-        wd.get("http://www.google.com");
-        Thread.sleep(10000);
-        BaseHtmlReport.createScreenshots(wd);
-        wd.get("http://www.baidu.com");
-        Thread.sleep(5000);
-        Assert.assertTrue(false);
-    }
+	@BeforeMethod
+	public void init() {
+		wd = DriverFactory.createNewDriver();
+	}
 
-    @AfterMethod
-    public void destory() {
-        DriverFactory.CloseDriver();
-    }
+	// @Test
+	public void MyfristTest5() throws InterruptedException {
+		wd.get("http://www.jiedaibao.com/pcIndex.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://qiye.jiedaibao.com/#/home");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://chongzhi.jiedaibao.com/");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://www.jiedaibao.com/functionDetail.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://www.jiedaibao.com/pcNewsList.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://www.jiedaibao.com/partials/aboutUs/promoterApply.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://www.jiedaibao.com/pcAbout.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("http://www.jiedaibao.com/pcIndex.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://qiye.jiedaibao.com/#/home");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://chongzhi.jiedaibao.com/");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://www.jiedaibao.com/functionDetail.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://www.jiedaibao.com/pcNewsList.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://www.jiedaibao.com/partials/aboutUs/promoterApply.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://www.jiedaibao.com/pcAbout.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+	}
+
+	@Test
+	public void MyfristTest6() throws InterruptedException {
+		wd.get("http://www.baidu.com");
+		Thread.sleep(3000);
+		wd.findElement(By.id("kw")).sendKeys("CoderInfo");
+		wd.findElement(By.id("su")).click();
+		// searchTextField.sendKeys("Symbio大傻逼公司");
+		// clickButton.click();
+		// BaseHtmlReport.createScreenshots(wd);
+		// wd.get("http://www.bing.com");
+		// Thread.sleep(3000);
+		// bSearchTextField.sendKeys("Symbio大傻逼公司");
+		// bClickButton.click();
+		// BaseHtmlReport.createScreenshots(wd);
+
+	}
+
+	// @Test
+	public void MyfristTest7() throws InterruptedException {
+
+		wd.get("http://www.jiedaibao.com/pcIndex.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://qiye.jiedaibao.com/#/home");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://chongzhi.jiedaibao.com/");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://www.jiedaibao.com/functionDetail.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://www.jiedaibao.com/pcNewsList.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://www.jiedaibao.com/partials/aboutUs/promoterApply.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		wd.get("https://www.jiedaibao.com/pcAbout.html");
+		Thread.sleep(5000);
+		BaseHtmlReport.createScreenshots(wd);
+		Assert.assertTrue(false);
+	}
+
+	@AfterMethod
+	public void destory() {
+		DriverFactory.CloseDriver();
+	}
 
 }
