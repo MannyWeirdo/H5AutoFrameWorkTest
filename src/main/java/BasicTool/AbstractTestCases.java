ccf7711cb4005faa4880c15a7bb3632a70091dda
Alex Yang <yu.yang@symbio.com>
2016-06-13 14:15:03 +0800

    create a new abstract parent class

--- src/main/java/BasicTool/AbstractTestCases.java
+++ src/main/java/BasicTool/AbstractTestCases.java
@@ -1,5 +1,29 @@
 package BasicTool;
 
-public abstract class AbstractTestCases {
+import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
+public abstract class AbstractTestCases {
+    public static WebDriver wd;
+
+    @BeforeMethod
+    public void initDriver() {
+        wd = DriverFactory.createNewDriver();
+    }
+
+    @AfterMethod
+    public void destoryDriver() {
+        DriverFactory.CloseDriver();
+    }
+
+    /**
+     * log message to TestNG result
+     * 
+     * @param str
+     */
+    public static void logMessage(String str) {
+        Reporter.log(str, 5, true); // Making a log entry.
+    }
 }
--- src/test/java/BasicTool/Mytest.java
+++ src/test/java/BasicTool/Mytest.java
@@ -1,116 +1,98 @@
 package BasicTool;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Mytest {
-	public WebDriver wd;
+public class Mytest extends AbstractTestCases {
+    // @Test
+    public void MyfristTest5() throws InterruptedException {
+        wd.get("http://www.jiedaibao.com/pcIndex.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://qiye.jiedaibao.com/#/home");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://chongzhi.jiedaibao.com/");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://www.jiedaibao.com/functionDetail.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://www.jiedaibao.com/pcNewsList.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://www.jiedaibao.com/partials/aboutUs/promoterApply.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://www.jiedaibao.com/pcAbout.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("http://www.jiedaibao.com/pcIndex.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://qiye.jiedaibao.com/#/home");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://chongzhi.jiedaibao.com/");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://www.jiedaibao.com/functionDetail.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://www.jiedaibao.com/pcNewsList.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://www.jiedaibao.com/partials/aboutUs/promoterApply.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://www.jiedaibao.com/pcAbout.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+    }
 
-	@BeforeMethod
-	public void init() {
-		wd = DriverFactory.createNewDriver();
-	}
-	// @Test
-	public void MyfristTest5() throws InterruptedException {
-		wd.get("http://www.jiedaibao.com/pcIndex.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://qiye.jiedaibao.com/#/home");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://chongzhi.jiedaibao.com/");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://www.jiedaibao.com/functionDetail.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://www.jiedaibao.com/pcNewsList.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://www.jiedaibao.com/partials/aboutUs/promoterApply.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://www.jiedaibao.com/pcAbout.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("http://www.jiedaibao.com/pcIndex.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://qiye.jiedaibao.com/#/home");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://chongzhi.jiedaibao.com/");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://www.jiedaibao.com/functionDetail.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://www.jiedaibao.com/pcNewsList.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://www.jiedaibao.com/partials/aboutUs/promoterApply.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://www.jiedaibao.com/pcAbout.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-	}
+    @Test
+    public void MyfristTest6() throws InterruptedException {
+        wd.get("http://www.baidu.com");
+        baiduPage page = new baiduPage(wd);
+        page.MyTestFunction();
+        // wd.findElement(By.id("kw")).sendKeys("CoderInfo");
+        // wd.findElement(By.id("su")).click();
+        // searchTextField.sendKeys("Symbio大傻逼公司");
+        // clickButton.click();
+        // BaseHtmlReport.createScreenshots(wd);
+        // wd.get("http://www.bing.com");
+        // Thread.sleep(3000);
+        // bSearchTextField.sendKeys("Symbio大傻逼公司");
+        // bClickButton.click();
+        // BaseHtmlReport.createScreenshots(wd);
 
-	@Test
-	public void MyfristTest6() throws InterruptedException {
-		wd.get("http://www.baidu.com");
-		baiduPage page = new baiduPage(wd);
-		page.MyTestFunction();
-//		wd.findElement(By.id("kw")).sendKeys("CoderInfo");
-//		wd.findElement(By.id("su")).click();
-		// searchTextField.sendKeys("Symbio大傻逼公司");
-		// clickButton.click();
-		// BaseHtmlReport.createScreenshots(wd);
-		// wd.get("http://www.bing.com");
-		// Thread.sleep(3000);
-		// bSearchTextField.sendKeys("Symbio大傻逼公司");
-		// bClickButton.click();
-		// BaseHtmlReport.createScreenshots(wd);
+    }
 
-	}
+     @Test
+    public void MyfristTest7() throws InterruptedException {
 
-	// @Test
-	public void MyfristTest7() throws InterruptedException {
-
-		wd.get("http://www.jiedaibao.com/pcIndex.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://qiye.jiedaibao.com/#/home");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://chongzhi.jiedaibao.com/");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://www.jiedaibao.com/functionDetail.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://www.jiedaibao.com/pcNewsList.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://www.jiedaibao.com/partials/aboutUs/promoterApply.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		wd.get("https://www.jiedaibao.com/pcAbout.html");
-		Thread.sleep(5000);
-		BaseHtmlReport.createScreenshots(wd);
-		Assert.assertTrue(false);
-	}
-
-	@AfterMethod
-	public void destory() {
-		DriverFactory.CloseDriver();
-	}
+        wd.get("http://www.jiedaibao.com/pcIndex.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://qiye.jiedaibao.com/#/home");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://chongzhi.jiedaibao.com/");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://www.jiedaibao.com/functionDetail.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://www.jiedaibao.com/pcNewsList.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://www.jiedaibao.com/partials/aboutUs/promoterApply.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        wd.get("https://www.jiedaibao.com/pcAbout.html");
+        Thread.sleep(5000);
+        BaseHtmlReport.createScreenshots(wd);
+        Assert.assertTrue(false);
+    }
 
 }
