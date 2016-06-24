b7f316673db1f409a4fd26966c560cef9ab2c953
Alex Yang <yu.yang@symbio.com>
2016-06-24 17:28:40 +0800

    use a new report

--- src/test/java/BasicTool/Mytest.java
+++ src/test/java/BasicTool/Mytest.java
@@ -5,49 +5,99 @@ import org.testng.annotations.Test;
 import Page.FunctionDetailPage;
 import Page.NavigationPage;
 import Page.PaymentPage;
-import Page.Mytest.TestPage;
-import Report.BaseHtmlReport;
 import TestCases.AbstractTestCases;
+
+import com.intuit.tools.reporter.WebReporter;
 
 public class Mytest extends AbstractTestCases {
 
-    @Test(testName = "1256", groups = { "Payment" }, description = "")
+    // @Test(testName = "1256", groups = { "Payment" }, description = "")
     public void MyfristTest6() throws Exception {
-        driver.get("http://www.jiedaibao.com/pcIndex.html");
-        BaseHtmlReport.createScreenshots(driver);
-        NavigationPage navigationPage = new NavigationPage(driver);
-        navigationPage.clickFunctionDetaillLink();
-        BaseHtmlReport.createScreenshots(driver);
-        navigationPage.clickPayPageLink();
-        BaseHtmlReport.createScreenshots(driver);
-        PaymentPage paymentPage = new PaymentPage(driver);
-        TestPage m = new TestPage(driver);
-
     }
 
-    @Test(testName = "12562", groups = { "Payment" })
+    // @Test(testName = "12562", groups = { "Payment" })
     public void MyfristTest7() throws Exception {
-        driver.get("http://www.jiedaibao.com/pcIndex.html");
-        BaseHtmlReport.createScreenshots(driver);
-        NavigationPage navigationPage = new NavigationPage(driver);
-        navigationPage.clickFunctionDetaillLink();
-        BaseHtmlReport.createScreenshots(driver);
-        navigationPage.clickPayPageLink();
-        BaseHtmlReport.createScreenshots(driver);
-        PaymentPage paymentPage = new PaymentPage(driver);
-        TestPage m = new TestPage(driver);
-
     }
 
-    @Test(testName = "1256", groups = { "Home" })
+    //@Test(testName = "1256", groups = { "Home" })
     public void MyfristTest9() throws Exception {
-        BaseHtmlReport.createScreenshots(driver);
+        WebReporter.log(driver, driver.getTitle(), true, true);
         NavigationPage navigationPage = new NavigationPage(driver);
         FunctionDetailPage functionDetailPage = navigationPage.clickFunctionDetaillLink();
-        BaseHtmlReport.createScreenshots(driver);
+        WebReporter.log(driver, driver.getTitle(), true, true);
         PaymentPage paymentPage = navigationPage.clickPayPageLink();
-        BaseHtmlReport.createScreenshots(driver);
-        TestPage m = new TestPage(driver);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        // TestPage m = new TestPage(driver);
+
+    }
+    
+    @Test(testName = "1256", groups = { "Home" })
+    public void MyfristTest10() throws Exception {
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        NavigationPage navigationPage = new NavigationPage(driver);
+        FunctionDetailPage functionDetailPage = navigationPage.clickFunctionDetaillLink();
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        PaymentPage paymentPage = navigationPage.clickPayPageLink();
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        WebReporter.log(driver, driver.getTitle(), true, true);
+
+        WebReporter.log(driver, driver.getTitle(), true, true);
+        // TestPage m = new TestPage(driver);
 
     }
 
