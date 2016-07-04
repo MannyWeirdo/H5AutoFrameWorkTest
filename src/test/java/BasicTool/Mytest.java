f6440941939de9c5336f13a498c1fdc68e9de453
杨宇 <yangyu@yangyudeMacBook-Pro.local>
2016-07-04 21:39:01 +0800

    update testcase and clean test_config file

--- src/test/java/BasicTool/Mytest.java
+++ src/test/java/BasicTool/Mytest.java
@@ -1,13 +1,16 @@
+
 package BasicTool;
 
 import org.testng.annotations.Test;
 
+import com.customize.reporter.WebReporter;
+
+import Page.BusinessVersionPage;
 import Page.FunctionDetailPage;
 import Page.NavigationPage;
+import Page.PCHomePage;
 import Page.PaymentPage;
 import TestCases.AbstractTestCases;
-
-import com.customize.reporter.WebReporter;
 
 public class Mytest extends AbstractTestCases {
 
@@ -62,43 +65,16 @@ public class Mytest extends AbstractTestCases {
     
     @Test(testName = "1256", groups = { "Home" })
     public void MyfristTest10() throws Exception {
-        WebReporter.log(driver, driver.getTitle(), true, true);
+        PCHomePage pCHomePage = new PCHomePage(driver);
         NavigationPage navigationPage = new NavigationPage(driver);
-        FunctionDetailPage functionDetailPage = navigationPage.clickFunctionDetaillLink();
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        PaymentPage paymentPage = navigationPage.clickPayPageLink();
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
+        navigationPage.clickNewListLink();
+        navigationPage.clickFunctionDetaillLink();
+        navigationPage.clickPromotionAndCooperationLink();
+        navigationPage.clickAboutWeLink();
+        navigationPage.clickPayPageLink();
+        navigationPage.clickBusinessVersionLink();
 
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        // TestPage m = new TestPage(driver);
-
+      
     }
 
 }
