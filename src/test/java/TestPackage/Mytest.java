2d61b6883dd1e0478245f68bca8661b6f3bc02c3
Alex <2722223@qq.com>
2016-07-14 10:43:16 +0800

    update testcase

--- src/test/java/TestPackage/Mytest.java
+++ src/test/java/TestPackage/Mytest.java
@@ -11,7 +11,7 @@ import TestCases.AbstractTestCases;
 
 public class Mytest extends AbstractTestCases {
 
-    // @Test(testName = "1256", groups = { "Home" })
+    @Test(testName = "1256", groups = { "Home" })
     public void MyfristTest10() throws Exception {
         System.out.println("********MyfristTest10 HOME************");
         PCHomePage pCHomePage = new PCHomePage(driver);
@@ -25,7 +25,7 @@ public class Mytest extends AbstractTestCases {
 
     }
 
-    // @Test(testName = "1257", groups = { "Payment" })
+    @Test(testName = "1257", groups = { "Payment" })
     public void MyfristTest11() throws Exception {
         System.out.println("********MyfristTest11 Payment************");
         PCHomePage pCHomePage = new PCHomePage(driver);
@@ -38,7 +38,7 @@ public class Mytest extends AbstractTestCases {
         navigationPage.clickBusinessVersionLink();
     }
 
-    // @Test(testName = "1257", groups = { "Home" })
+    @Test(testName = "1257", groups = { "Home" })
     public void MyfristTest12() throws Exception {
         System.out.println("********MyfristTest12 HOME************");
         PCHomePage pCHomePage = new PCHomePage(driver);
@@ -51,7 +51,7 @@ public class Mytest extends AbstractTestCases {
         navigationPage.clickBusinessVersionLink();
     }
 
-    @Test(testName = "1258", groups = { "Home" })
+    // @Test(testName = "1258", groups = { "Home" })
     public void MyfristTest13() throws Exception {
         System.out.println("********MyfristTest13 HOME************");
         TestPage testPage = new TestPage(driver);
