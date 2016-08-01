a80ca0922cf3685d268f3ecf68a2f91d2c345886
Alex <2722223@qq.com>
2016-08-01 21:33:49 +0800

    update cases and test_config.properties

--- src/test/java/TestPackage/paymentPageTest/PaymentLoginTest.java
+++ src/test/java/TestPackage/paymentPageTest/PaymentLoginTest.java
@@ -1,21 +1,33 @@
 package TestPackage.paymentPageTest;
 
 import org.testng.annotations.Test;
-import Page.Payment.PaymentBalancePage;
+
 import com.customize.reporter.WebReporter;
 
-public class PaymentLoginTest extends PaymentPageBaseTest {
-    @Test(testName = "", description = "在线充值IOS扫描二维码登录")
+import BasicTool.Config.ConfigUtil;
+import Page.NavigationPage;
+import Page.Payment.PaymentBalancePage;
+import Page.Payment.PaymentLoginPage;
+import TestCases.AbstractTestCases;
+
+public class PaymentLoginTest extends AbstractTestCases {
+    // @Test(testName = "", description = "在线充值IOS扫描二维码登录")
     public void scanQrcodeLoginFromIOSTest() throws Exception {
-    	PaymentBalancePage balancePage = loginPaymentPage("IOS");
-    	balancePage.logout();
+        ConfigUtil.getConfigUtil().getTestConfig().setProperty("phonePlatform", "IOS");
+        NavigationPage navigationPage = new NavigationPage(driver);
+        PaymentLoginPage paymentLoginPage = navigationPage.clickPayPageLink();
+        PaymentBalancePage balancePage = paymentLoginPage.loginPaymentPage("IOS");
+        balancePage.logout();
         WebReporter.log(driver, driver.getTitle(), true, true);
     }
-    
+
     @Test(testName = "", description = "在线充值Android扫描二维码登录")
     public void scanQrcodeLoginFromAndroidTest() throws Exception {
-    	PaymentBalancePage balancePage = loginPaymentPage("Android");
-    	balancePage.logout();
+        ConfigUtil.getConfigUtil().getTestConfig().setProperty("phonePlatform", "ANDRIOD");
+        NavigationPage navigationPage = new NavigationPage(driver);
+        PaymentLoginPage paymentLoginPage = navigationPage.clickPayPageLink();
+        PaymentBalancePage balancePage = paymentLoginPage.loginPaymentPage("ANDRIOD");
+        balancePage.logout();
         WebReporter.log(driver, driver.getTitle(), true, true);
     }
 }
