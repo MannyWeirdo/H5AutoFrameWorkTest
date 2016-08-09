3ac5a85a9ec77896050fd79bdb7d8c0fbc3f75d7
duyw <duyw@jiedaibao.com>
2016-08-09 11:25:11 +0800

    update TestPromoterApply

--- src/test/java/TestPackage/opromoterApply/TestPromoterApply.java
+++ src/test/java/TestPackage/opromoterApply/TestPromoterApply.java
@@ -15,22 +15,21 @@ public class TestPromoterApply extends AbstractTestCases {
 
     @Test(description = "申请为推广商个人版")
     public void testPersonPromoterApply() throws Exception {
-
         navigationPage = new NavigationPage(driver);
         opromoterApplyPage = navigationPage.clickPromoterApplyLink();
         opromoterBean = DataFactory.GetData(OpromoterBean.class);
-        opromoterApplyPage.applyForPersonOpromoter(opromoterBean);
-        opromoterApplyPage.clickSubmitButton();
+        opromoterApplyPage.fillOpromoterInformation(opromoterBean);
+        opromoterApplyPage.clickSubmitButton(); 
     }
 
     // @Test(description = "申请为推广商企业版")
     public void testCompanyPromoterApply() throws Exception {
         navigationPage = new NavigationPage(driver);
-        opromoterBean = new OpromoterBean("tester", "612657189910110389", "19100000012", "test@jiedaibao.com", "北京",
-                "沙河", "cooperSource");
+        opromoterBean = DataFactory.GetData(OpromoterBean.class);
+        opromoterBean.setPromotionDirection(1);
         opromoterApplyPage = navigationPage.clickPromoterApplyLink();
-        opromoterApplyPage.applyForCompanyOpromoter(opromoterBean);
-        // opromoterApplyPage.clickSubmitButton();
+        opromoterApplyPage.fillOpromoterInformation(opromoterBean);
+        opromoterApplyPage.clickSubmitButton();
     }
 
     // @Test(description = "个人版企业版连接")
