f313aa7fe61a062c246e7948a79de9ee5adfbd94
Alex <2722223@qq.com>
2016-09-09 13:50:16 +0800

    resovle conflact

--- src/test/java/testPackage/pcNewsListPageList/CompanyNewsTest.java
+++ src/test/java/testPackage/pcNewsListPageList/CompanyNewsTest.java
@@ -9,16 +9,13 @@ import page.pc.newListPage.NewListPage;
 import testCases.AbstractTestCases;
 
 public class CompanyNewsTest extends AbstractTestCases {
-	
-	
-	
-	@Test(testName = "",description = "信息动态点进公司新闻")
-	public void intoCompanyNewList() throws Exception{
-		NavigationPage navigationPage = new NavigationPage(driver);
-		NewListPage newListPage = navigationPage.clickNewListLink();
-		newListPage.clickcompanyNewsList();
-		WebReporter.log(driver, driver.getTitle(), true, true);
-	}
-	
-	
+
+    @Test(testName = "", description = "信息动态点进公司新闻")
+    public void intoCompanyNewList() throws Exception {
+        NavigationPage navigationPage = new NavigationPage(driver);
+        NewListPage newListPage = navigationPage.clickNewListLink();
+        newListPage.clickcompanyNewsList();
+        WebReporter.log(driver, driver.getTitle(), true, true);
+    }
+
 }
--- src/test/java/testPackage/pcNewsListPageList/ProductNewsTest.java
+++ src/test/java/testPackage/pcNewsListPageList/ProductNewsTest.java
@@ -2,23 +2,20 @@ package testPackage.pcNewsListPageList;
 
 import org.testng.annotations.Test;
 
+import com.customize.reporter.WebReporter;
+
 import page.pc.NavigationPage;
 import page.pc.newListPage.NewListPage;
 import testCases.AbstractTestCases;
 
-import com.customize.reporter.WebReporter;
+public class ProductNewsTest extends AbstractTestCases {
 
-public class ProductNewsTest extends AbstractTestCases{
-	
-	@Test(testName = "",description = "信息动态点进产品新闻")
-	public void intoProductNewList() throws Exception{
-		NavigationPage navigationPage = new NavigationPage(driver);
-		NewListPage newListPage = navigationPage.clickNewListLink();
-		newListPage.clickProductNewsList();
-		WebReporter.log(driver, driver.getTitle(), true, true);
-	}
-	
+    @Test(testName = "", description = "信息动态点进产品新闻")
+    public void intoProductNewList() throws Exception {
+        NavigationPage navigationPage = new NavigationPage(driver);
+        NewListPage newListPage = navigationPage.clickNewListLink();
+        newListPage.clickProductNewsList();
+        WebReporter.log(driver, driver.getTitle(), true, true);
+    }
+
 }
-
-
-
