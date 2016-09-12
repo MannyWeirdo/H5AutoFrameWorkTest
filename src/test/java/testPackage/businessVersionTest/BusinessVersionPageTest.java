4fc97df1d7129ff7091ecac3314023051f721f89
Alex <2722223@qq.com>
2016-09-12 17:38:44 +0800

    update @test

--- src/test/java/testPackage/businessVersionTest/BusinessVersionPageTest.java
+++ src/test/java/testPackage/businessVersionTest/BusinessVersionPageTest.java
@@ -8,31 +8,31 @@ import page.pc.NavigationPage;
 import page.pc.businessVersionPage.BusinessVersionPage;
 import testCases.AbstractTestCases;
 
+public class BusinessVersionPageTest extends AbstractTestCases {
 
-public class BusinessVersionPageTest extends AbstractTestCases{
-	
-	//Test(testName = "1", description = "璺宠浆鍒颁紒涓氭敞鍐岄〉闈�)
-	public void clickSignUpOne() throws Exception{
-		NavigationPage navigationPage = new NavigationPage(driver);
-		BusinessVersionPage businessVerPage = navigationPage.clickBusinessVersionLink();
-		businessVerPage.clickSignUpOne();
-		WebReporter.log(driver, driver.getTitle(), true, true);
-	}
-	
-	//@Test(testName = "2", description = "宸叉湁璐﹀彿锛岀洿鎺ョ櫥褰�)
-	public void clickLoginLink() throws Exception{
-		NavigationPage navigationPage = new NavigationPage(driver);
-		BusinessVersionPage businessVerPage = navigationPage.clickBusinessVersionLink();
-		businessVerPage.clickLoginLink();
-		WebReporter.log(driver, driver.getTitle(), true, true);
-	}
-	
-	@Test(testName = "3", description = "浜嗚В鏇村")
-	public void clickKnowMoreButton() throws Exception{
-		NavigationPage navigationPage = new NavigationPage(driver);
-		BusinessVersionPage businessVerPage = navigationPage.clickBusinessVersionLink();
-		businessVerPage.clickKnowMoreButton();
-		WebReporter.log(driver, driver.getTitle(), true, true);
-	}
+    @Test(testName = "1", description = "点击注册button，跳转注册企业账户页面")
+
+    public void clickSignUpOne() throws Exception {
+        NavigationPage navigationPage = new NavigationPage(driver);
+        BusinessVersionPage businessVerPage = navigationPage.clickBusinessVersionLink();
+        businessVerPage.clickSignUpOne();
+        WebReporter.log(driver, driver.getTitle(), true, true);
+    }
+
+    @Test(testName = "2", description = "已有账户，直接登录")
+    public void clickLoginLink() throws Exception {
+        NavigationPage navigationPage = new NavigationPage(driver);
+        BusinessVersionPage businessVerPage = navigationPage.clickBusinessVersionLink();
+        businessVerPage.clickLoginLink();
+        WebReporter.log(driver, driver.getTitle(), true, true);
+    }
+
+    @Test(testName = "3", description = "纯信用融资了解更多")
+    public void clickKnowMoreButton() throws Exception {
+        NavigationPage navigationPage = new NavigationPage(driver);
+        BusinessVersionPage businessVerPage = navigationPage.clickBusinessVersionLink();
+        businessVerPage.clickKnowMoreButton();
+        WebReporter.log(driver, driver.getTitle(), true, true);
+    }
 
 }
