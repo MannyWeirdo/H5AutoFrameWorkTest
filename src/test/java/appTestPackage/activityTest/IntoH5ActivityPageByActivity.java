3be61d1f021888b418e1afcd69d0bd7630b319c2
Alex <2722223@qq.com>
2016-09-27 21:01:19 +0800

    add new app test cases for activity

--- src/test/java/appTestPackage/activityTest/IntoH5ActivityPageByActivity.java
+++ src/test/java/appTestPackage/activityTest/IntoH5ActivityPageByActivity.java
@@ -1,22 +1,22 @@
-
-package appTestPackage.moneyJournalTest;
+package appTestPackage.activityTest;
 
 import org.testng.annotations.Test;
 
 import com.customize.reporter.WebReporter;
 
 import basicTool.appLoginTool;
+import page.app.discover.ActivityPage;
 import page.app.discover.DiscoverPage;
 import page.app.navigation.NavigationPage;
 import testCases.AbstractTestCases;
 
-public class MoneyJournal extends AbstractTestCases {
-
-    @Test(testName = "MoneyJournal1", description = "进入央行征信", groups = "phone")
-    public void IntoBankCentralBankCredit() throws Exception {
+public class IntoH5ActivityPageByActivity extends AbstractTestCases {
+    @Test(testName = "Activity1", description = "进入央行征信", groups = "phone")
+    public void IntoTheFristH5ActivityPage() throws Exception {
         NavigationPage navigationPage = appLoginTool.appSimpleAccountLogin();
         DiscoverPage discoverPage = navigationPage.navigationToDiscover();
-        discoverPage.clickMoneyJournalButton();
+        ActivityPage activityPage = discoverPage.clickActivityButton();
+        activityPage.intoActivelPageByNum(0);
         WebReporter.log(appiumDirver, true, true, true);
     }
 
