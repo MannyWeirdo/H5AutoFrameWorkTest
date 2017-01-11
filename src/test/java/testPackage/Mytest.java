9b5d1691e37712fc71e657d32580059d8a1b18f0
Alex <alex@zhangranrandeMacBook-Pro.local>
2017-01-11 18:06:38 +0800

    for miaoqian

--- src/test/java/testPackage/Mytest.java
+++ src/test/java/testPackage/Mytest.java
@@ -3,31 +3,16 @@ package testPackage;
 
 import org.testng.annotations.Test;
 
-import com.customize.reporter.WebReporter;
-
 import dataBean.app.login.UserBean;
-import page.pc.NavigationPage;
-import page.pc.homePage.PCHomePage;
 import testCases.AbstractTestCases;
 
 public class Mytest extends AbstractTestCases {
 
-    UserBean userBean;
+	UserBean userBean;
 
-    @Test(testName = "1257")
-    public void MyfristTest12() throws Exception {
-        System.out.println("********MyfristTest12 HOME************");
-        PCHomePage pCHomePage = new PCHomePage(driver);
-        NavigationPage navigationPage = new NavigationPage(driver);
-        navigationPage.clickNewListLink();
-        navigationPage.clickFunctionDetaillLink();
-        navigationPage.clickPromoterApplyLink();
-        navigationPage.clickAboutUsLink();
-        navigationPage.clickPayPageLink();
-        navigationPage.clickBusinessVersionLink();
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        // Assert.assertTrue(false);
-
-    }
+	@Test(testName = "1257", description = "", groups = "pc")
+	public void MyfristTest12() throws Exception {
+		System.out.println("hahaha");
+	}
 
 }
