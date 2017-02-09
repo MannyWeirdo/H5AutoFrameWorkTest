00bd29a6df8bca7b7f16111eb20b6749624641a7
Alex Yang <yangy@shicaidai.com>
2017-02-09 10:21:08 +0800

    add some test cases

--- src/test/java/testPackage/Mytest.java
+++ src/test/java/testPackage/Mytest.java
@@ -4,9 +4,12 @@ package testPackage;
 import org.testng.annotations.Test;
 
 import com.customize.reporter.WebReporter;
-import com.customize.reporter.model.WebLog;
 
 import dataBean.app.login.UserBean;
+import page.pc.homePage.PCHomePage;
+import page.pc.livePage.LivePage;
+import page.pc.loginPage.LoginPage;
+import page.pc.navigationPage.NavigationPage;
 import testCases.AbstractTestCases;
 
 public class Mytest extends AbstractTestCases {
@@ -14,17 +17,21 @@ public class Mytest extends AbstractTestCases {
 	UserBean userBean;
 
     @Test(testName = "1257", description = "", groups = "pc")
-    public void MyfristTest12() throws Exception {
-        System.out.println("hahaha");
+    public void LoginTest() throws Exception {
+        PCHomePage pchomePage = new PCHomePage(driver);
+        LoginPage loginPage = pchomePage.gotoLoginPage();
+        pchomePage = loginPage.login("13295422111", "xiaoyang00x");
         WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
-        WebReporter.log(driver, driver.getTitle(), true, true);
+
+    }
+    
+    @Test(testName = "1258", description = "", groups = "pc")
+    public void gotoLivePage() throws Exception {
+        PCHomePage pchomePage = new PCHomePage(driver);
+        LoginPage loginPage = pchomePage.gotoLoginPage();
+        pchomePage = loginPage.login("13295422111", "xiaoyang00x");
+        NavigationPage navigationPage = new NavigationPage(driver);      
+        LivePage livePage = navigationPage.navigationLivePage();
         WebReporter.log(driver, driver.getTitle(), true, true);
     }
 }
